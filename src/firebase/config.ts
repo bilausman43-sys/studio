ad5b505a2330b8cb5deb926e0c15045c639d6135
Usman Bila <usmanshaka706@gmail.com>
2026-03-09 22:42:39 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9266122055-ecc9d",
+  "appId": "1:561842160037:web:c92db2d0fcb7587cdf284c",
+  "apiKey": "AIzaSyBaPaC0VUOuyTgVyZfHLl2hQZ2z_43bk4w",
+  "authDomain": "studio-9266122055-ecc9d.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "561842160037"
+};
